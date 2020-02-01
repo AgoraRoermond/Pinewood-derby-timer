a2b27805c15a4b39086d1baa1ca2078e47e308cf
itzpizzaboi <36034558+itzpizzaboi@users.noreply.github.com>
2020-02-01 09:12:29 +0100

    Update Pinewood_derpy_timer.ino

--- Pinewood_derpy_timer.ino
+++ Pinewood_derpy_timer.ino
@@ -2,15 +2,15 @@
 #include <Wire.h>
 
 
-#define ButtonUp 2
-#define ButtonDown 6 
-#define ButtonPush 4
-#define maxTime 60
-#define sensitivity 50
-#define buttonG 8
-#define Sensor0 A0
-#define Sensor1 A1
-#define Sensor2 A2
+#define ButtonUp 6
+#define ButtonDown 4
+#define ButtonPush 2
+#define maxTime 60      // the DNF timer in sec
+#define sensitivity 100 //how sensitive the car sensor is
+#define buttonG 8   //begin button
+#define Sensor0 A8  //car 1 sensor
+#define Sensor1 A9  //car 2 sensor
+#define Sensor2 A10 //car 3 sensor
 
 unsigned long begintime = 0;
 
@@ -75,18 +75,15 @@ String Buttons(void)
   bool Up = digitalRead(ButtonUp);
   bool Down = digitalRead(ButtonDown);
 
-  if (Up != OldUp && Up == HIGH)
-  {
+  if (Up != OldUp && Up == HIGH) {
     Rotation = "up";
     // bSerial.println(Rotation);
   }
-  else if (Down != OldDown && Down == HIGH)
-  {
+  else if (Down != OldDown && Down == HIGH) {
     Rotation = "down";
     //Serial.println(Rotation);
   }
-  else
-  {
+  else {
     Rotation = "NULL";
   }
 
@@ -97,23 +94,18 @@ String Buttons(void)
 
 
 
-void UpdateScreen(String Rotation)
-{
-  if (Rotation.equals("up"))
-  {
+void UpdateScreen(String Rotation) {
+  if (Rotation.equals("up")) {
     MenuState = MenuState + 1;
   }
-  else if (Rotation.equals("down"))
-  {
+  else if (Rotation.equals("down")) {
     MenuState = MenuState - 1;
   }
 
-  if (MenuState > 4)
-  {
+  if (MenuState > 4) {
     MenuState = 1;
   }
-  else if (MenuState < 1)
-  {
+  else if (MenuState < 1) {
     MenuState = 4;
   }
 
@@ -127,7 +119,7 @@ void UpdateScreen(String Rotation)
     bool Button = HIGH;
   }
 
-  Serial.println(Button);
+  //Serial.println(Button);
   OldPush = Push;
 
   if (MenuState != OldMenuState or Button == LOW) {
@@ -137,42 +129,37 @@ void UpdateScreen(String Rotation)
   }
 
 
-  switch (MenuState)
-  {
+  switch (MenuState) {
     case 1:
       lcd.setCursor(0, 0);
       lcd.print(">");
       if (Button == LOW) {
-        if (MenuSelect == 0)
-        {
+        if (MenuSelect == 0) {
           MenuSelect = 1;
         }
-
       }
       break;
     case 2:
       lcd.setCursor(0, 1);
       lcd.print(">");
       if (Button == 0) {
-        if (MenuSelect == 0)
-        {
+        if (MenuSelect == 0) {
           MenuSelect = 2;
         }
-        else if (MenuSelect == 1)
-        {
+        else if (MenuSelect == 1) {
           lcd.setCursor(3, 1);
           lcd.print("Recording...");
-          //Record();
-          FakeRecord();
+          Record();
+          //FakeRecord();
           lcd.clear();
           lcd.setCursor(3, 1);
           lcd.print("Done");
-          delay(3000);
+          delay(1000);
           lcd.clear();
           lcd.setCursor(3, 1);
-          lcd.print("Score are saved");
-          delay(3000);
-          lcd.clear();                  
+          lcd.print("Score's are saved");
+          delay(1000);
+          lcd.clear();
         }
       }
       break;
@@ -180,8 +167,7 @@ void UpdateScreen(String Rotation)
       lcd.setCursor(0, 2);
       lcd.print(">");
       if (Button == 0) {
-        if (MenuSelect == 0)
-        {
+        if (MenuSelect == 0) {
           MenuSelect = 3;
         }
       }
@@ -190,9 +176,19 @@ void UpdateScreen(String Rotation)
       lcd.setCursor(0, 3);
       lcd.print(">");
       if (Button == 0) {
-        if (MenuSelect == 1 or MenuSelect == 2 or MenuSelect == 3)
-        {
+        if (MenuSelect == 0) {
+          baseLight0 = analogRead(Sensor0);
+          baseLight1 = analogRead(Sensor1);
+          baseLight2 = analogRead(Sensor2);
+          lcd.clear();
+          lcd.setCursor(3, 1);
+          lcd.print("sensors are reset");
+          delay(3000);
+          lcd.clear();
+        }
+        if (MenuSelect == 1 or MenuSelect == 2 or MenuSelect == 3) {
           MenuSelect = 0;
+          delay(100);
         }
       }
       break;
@@ -205,6 +201,8 @@ void UpdateScreen(String Rotation)
     lcd.print(" >Score's Race<");
     lcd.setCursor(2, 2);
     lcd.print(" >Web adress<");
+    lcd.setCursor(2, 3);
+    lcd.print(" >Reset Sensors<");
   }
   else if (MenuSelect == 1) {
     lcd.setCursor(3, 1);
@@ -215,21 +213,21 @@ void UpdateScreen(String Rotation)
   else if (MenuSelect == 2) {
     //score car 1
     lcd.setCursor(3, 0);
-    lcd.print("car 1 = ");
+    lcd.print("Car 1 = ");
     lcd.setCursor(11, 0);
     lcd.print(endTime0 / 1000);
     lcd.setCursor(17, 0);
     lcd.print("sec");
     //score car 2
     lcd.setCursor(3, 1);
-    lcd.print("car 2 = ");
+    lcd.print("Car 2 = ");
     lcd.setCursor(11, 1);
     lcd.print(endTime1 / 1000);
     lcd.setCursor(17, 1);
     lcd.print("sec");
     //score car 3
     lcd.setCursor(3, 2);
-    lcd.print("car 3 = ");
+    lcd.print("Car 3 = ");
     lcd.setCursor(11, 2);
     lcd.print(endTime2 / 1000);
     lcd.setCursor(17, 2);
@@ -272,6 +270,9 @@ void Record() {
     //start de timer
     if (sensorBegin == LOW and beginTimeDone == false) {
       begintime = millis();
+      lcd.clear();
+      lcd.setCursor(3, 1);
+      lcd.print("Start");
       Serial.println("recording...");
       beginTimeDone = true;
     }
@@ -310,6 +311,16 @@ void Record() {
 
     if (DeltaTime > maxTime and beginTimeDone == true or endTime0 > 0 and endTime1 > 0 and endTime2 > 0 and beginTimeDone == true) {
       recording = false;
+      if (endTime0 == 0) {
+        endTime0 == NULL;
+      }
+      if (endTime1 == 0) {
+        endTime1 == NULL;
+      }
+      if (endTime2 == 0) {
+        endTime2 == NULL;
+      }
+
       /*    Serial.println("done");
             Serial.print(endTime0 / 1000);
             Serial.println(" sec");
@@ -321,9 +332,9 @@ void Record() {
   }
 }
 
-void FakeRecord(){
-delay(3000);
-endTime0 = random(1000, 30000);
-endTime1 = random(1000, 30000);
-endTime2 = random(1000, 30000);
-}
+/*void FakeRecord() {
+  delay(3000);
+  endTime0 = random(1000, 30000);
+  endTime1 = random(1000, 30000);
+  endTime2 = random(1000, 30000);
+  }*/
