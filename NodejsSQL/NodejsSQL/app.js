661934ef6a43edc061f582da244dbc807d46c09e
Android Unity <unconfigured@null.spigotmc.org>
2020-03-23 11:13:23 +0100

    Added SQL functionalities
    MariaDB based code, should work with MySQL aswell. Code can display studentime from the table student

--- NodejsSQL/NodejsSQL/app.js
+++ NodejsSQL/NodejsSQL/app.js
@@ -2,35 +2,40 @@ var app = require('express')();
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
 
-var test;
+var student;
 
 var connection = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
         password: "AgoraRoermond",
-        database: "test"
+        database: "Derby"
     });
+
 
 connection.connect(function (err) {
     if (err) throw err;
-    connection.query("SELECT * FROM leerling", function (err, result, fields) {
+    connection.query("SELECT * FROM student", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
-        test = fields[0].toString();
         console.log(test);
+        student = result;
     }
     )
 })
 
+connection.query("SELECT * FROM student"(result, fields) => { student = result; });
+console.log(student);
+
 var port = 80;
-//var test = "hello there";
+var test = "hello there";
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/public");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => { res.render("index", { test: test }); });
+
+app.get("/", (req, res) => { res.render("index", { test: student[0].studenttime }); });
 app.get("/OwO", (req, res) => { res.render("pageA", { test: test }); });
 app.get("/easteregg", (req, res) => { res.render("secret", { test: test }); });
 app.get("*", (req, res) => { res.render("404", { test: test }); });
