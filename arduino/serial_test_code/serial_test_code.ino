b35ed771d28e013ded902e7269a2a5d4b1018d55
itzpizzaboi <36034558+itzpizzaboi@users.noreply.github.com>
2020-05-27 13:38:56 +0200

    updated the arduino test code so it is cleaner

--- arduino/serial_test_code/serial_test_code.ino
+++ arduino/serial_test_code/serial_test_code.ino
@@ -1,27 +1,11 @@
 float floatA = 1.52;
 float floatB = 8.12;
 float floatC = 0.13;
-String stringA;
-String stringB;
-String stringC;
 String endString;
 
 
 void setup() {
   Serial.begin(115200);
-  stringA = floatA;
-  stringB = floatB;
-  stringC = floatC;
-
-
-  //creates the string bcus arduino is stupid
-//  endString = '#';
-//  endString = endString+stringA;
-//  endString = endString+'&';
-//  endString = endString+stringB; 
-//  endString = endString+'&';
-//  endString = endString+stringC;
-//  endString = endString+'*';
   endString = String(floatA)+"&"+String(floatB)+"&"+String(floatC);
 }
 
