03c1109581593704f2bcea8dfbd241407c2b3c8f
davidmaenen <davidmaenen@agoraroermond.nl>
2020-06-12 11:03:50 +0200

    table times aagepast met studentId

--- setup.sql
+++ setup.sql
@@ -9,7 +9,11 @@ INSERT INTO `students` (`name`,`class`) VALUES ('Jetse','VWO5'),('Martijn','VWO5
 
 DROP TABLE IF EXISTS `times`;
 CREATE TABLE times (
+<<<<<<< Updated upstream
   `id` int(11) NOT NULL AUTO_INCREMENT,
+=======
+  `studentId` int(11) NOT NULL AUTO_INCREMENT,
+>>>>>>> Stashed changes
   `time` float(45) DEFAULT NULL,
   `attempt` float(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
