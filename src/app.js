eccfe8a179d3749450f5c46a3021126b17fc6b73
sjohnie-rgb <61227972+sjohnie-rgb@users.noreply.github.com>
2020-06-22 12:01:16 +0200

    Login form met sesions voor leerling
    hoop ik tenminste

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
 const routes = require('./router');
+const app = express ()
+const bodyParser = require("body-parser")
 require('./serial.js');
 
 const app = express();
@@ -10,10 +12,16 @@ var port = process.env.NODE_ENV === "production" ? 80 : 8080;
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
+app.get('/',(req, res) => {res.render('index')})
+app.post("/submit", (req, res) => {
+  console.log("Username: "+req.body.username)
+  res.redirect("/")
+})
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
 app.use('/', routes);
+app.use(bodyparser.urlencoded({extended:true}))
 
 app.listen(port, () => console.log("Succesfully vibing on port: " + port));
