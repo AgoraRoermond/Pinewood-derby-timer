1ba216bc388215901b20be993e42af3c7c093cbf
itzpizzaboi <36034558+itzpizzaboi@users.noreply.github.com>
2020-06-26 09:36:31 +0200

    added query for adding student names UNSAFE

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,39 +1,48 @@
 const sql = require('../db.js');
 
 async function showTimes(request, response) {
-    allTimes = await sql.query("SELECT * FROM times");
-    response.render('pages/admin/allTimes', { allTimes });
+  allTimes = await sql.query("SELECT * FROM times");
+  response.render('pages/admin/allTimes', {
+    allTimes
+  });
 }
 async function showAcounts(request, response) {
-    acounts = await sql.query("SELECT * FROM times");
-    response.render('pages/admin/acounts', { acounts });
+  acounts = await sql.query("SELECT * FROM times");
+  response.render('pages/admin/acounts', {
+    acounts
+  });
 }
 async function getUser(request, response) {
-    getAcounts = await sql.query("SELECT studentId FROM times");
-    response.render('pages/admin/user', { getAcounts });
+  getAcounts = await sql.query("SELECT studentId FROM times");
+  response.render('pages/admin/user', {
+    getAcounts
+  });
 
 }
 async function userData(request, response) {
-    var userId = 10521;
-    userData = await sql.query("SELECT * FROM times WHERE id=?", [userId]);
-    response.render('pages/admin/user', { userData });
+  var userId = 10521;
+  userData = await sql.query("SELECT * FROM times WHERE id=?", [userId]);
+  response.render('pages/admin/user', {
+    userData
+  });
 }
 async function getNewUser(request, response) {
-    response.render('pages/admin/newUser');
+  response.render('pages/admin/newUser');
 
 }
 async function postNewUser(request, response) {
-    var studentName = request.body.studentName;
-    console.log(studentName);
-    response.redirect('/admin/user');
+  var studentName = request.body.studentName;
+  console.log(studentName);
+  await sql.query("INSERT INTO times(StudentName) VALUES(" + studentName + ")");
+  response.redirect('/admin/user');
 
 }
 
 module.exports = {
-    showTimes,
-    showAcounts,
-    getUser,
-    userData,
-    getNewUser,
-    postNewUser
+  showTimes,
+  showAcounts,
+  getUser,
+  userData,
+  getNewUser,
+  postNewUser
 };
