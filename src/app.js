816ff31ab67d237093e446782917549306734893
davidagoraroermond <59532635+davidagoraroermond@users.noreply.github.com>
2020-09-11 13:40:45 +0200

    session toe gevoegd

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const routes = require('./router');
 require('./serial.js');
-
+const session = require('express-session')
 const app = express();
 var port = process.env.NODE_ENV === "production" ? 80 : 8080;
 
@@ -11,9 +11,16 @@ app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
 app.use('/', routes);
 
 app.listen(port, () => console.log("Succesfully vibing on port: " + port));
