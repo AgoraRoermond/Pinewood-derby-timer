c92254cd2276fc195e57ed7851fcc43988309884
itzpizzaboi <36034558+itzpizzaboi@users.noreply.github.com>
2020-09-16 09:49:18 +0200

    added post request and the insirt sql query to the admin router and conntroller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -8,11 +8,23 @@ async function getTimes(request, response) {
   });
 }
 
-async function getAssignTimes(request, response) {
+function getAssignTimes(request, response) {
   var unassignedTimes = serial.getLatestTimes();
   return response.render('pages/admin/getAssignTimes', {
     unassignedTimes,
   });
+}
+
+async function postAssignTimes(request, response) {
+  var {studentMail0, studentMail1, studentMail2} = request.body;
+  var assignedTimes = serial.getLatestTimes();
+  // console.log(studentMail0);
+  // console.log(studentMail1);
+  // console.log(studentMail2);
+  await sql.query("INSERT INTO `times` (`student_mail`,`time`) VALUES (?,?);", [studentMail0, assignedTimes[0]]);
+  await sql.query("INSERT INTO `times` (`student_mail`,`time`) VALUES (?,?);", [studentMail1, assignedTimes[1]]);
+  await sql.query("INSERT INTO `times` (`student_mail`,`time`) VALUES (?,?);", [studentMail2, assignedTimes[2]]);
+  return response.render('pages/index');
 }
 
 
@@ -20,4 +32,5 @@ async function getAssignTimes(request, response) {
 module.exports = {
   getTimes,
   getAssignTimes,
+  postAssignTimes,
 }
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -4,5 +4,6 @@ const adminRouter = express.Router();
 
 adminRouter.get('/times', adminController.getTimes);
 adminRouter.get('/getAssignTimes', adminController.getAssignTimes);
+adminRouter.post('/getAssignTimes', adminController.postAssignTimes);
 
 module.exports = adminRouter;
