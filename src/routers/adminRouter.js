3a0578670658713b16058c9a1bbb6ddcd2c27ba0
itzpizzaboi <36034558+itzpizzaboi@users.noreply.github.com>
2020-09-16 13:31:21 +0200

    changed the naming of assignTimes

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -3,7 +3,7 @@ const adminController = require('../controllers/adminController');
 const adminRouter = express.Router();
 
 adminRouter.get('/times', adminController.getTimes);
-adminRouter.get('/AssignTimes', adminController.getAssignTimes);
-adminRouter.post('/AssignTimes', adminController.postAssignTimes);
+adminRouter.get('/assignTimes', adminController.getAssignTimes);
+adminRouter.post('/assignTimes', adminController.postAssignTimes);
 
 module.exports = adminRouter;
